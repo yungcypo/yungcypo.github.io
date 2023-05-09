0d312b885857868df24984bf8a24c5bd5b8b2a36
yungcypo <cypooriginal@gmail.com>
2023-05-09 15:51:05 +0200

    trying to publish

--- src/Head.tsx
+++ src/Head.tsx
@@ -1,15 +1,11 @@
-/* not working */
-import { Helmet } from 'react-helmet';
-
 function Head(){
-    return (<Helmet>
+    return (
+    <>
         <meta charSet="utf-8" />
         <title>Cypo</title>
         <link rel="icon" href="/assets/images/icons/favicon.ico" type="image/x-icon" />
-
-    
-
-    </Helmet>);
+    </>
+    );
 }
 
 export default Head;
